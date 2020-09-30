e0b1cba9500f44ab61d1ed71b6b98b7a5b973190
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-09-30 23:36:40 +0600

    IUT logo added

--- IUTapp/src/screen/Home.js
+++ IUTapp/src/screen/Home.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, View, Image,StyleSheet} from 'react-native';
 
 const HomeScreen =()=>{
 	return (
-		<Text style={ styles.textStyle }> Hello there!</Text>
+		<View style={styles.container}>
+			<Image style={styles.imageStyle}
+			source={require('./../../assets/iut_logo.png')}>
+			</Image>
+		</View>
 		);
 }
 
 const styles = StyleSheet.create(
 	{
+		container: {
+	    display: "flex",
+	    justifyContent: "space-around",
+	    alignItems: "center",
+	    height: "50%",
+	    textAlign: "center"
+	  },
 		textStyle:{
 			fontSize: 30,
 			color: 'blue',
 		},
+
+		imageStyle:{
+
+			height:165,
+			width: 100
+		}
 	}
 );
 
