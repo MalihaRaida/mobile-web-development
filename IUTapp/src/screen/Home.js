337300a4d77fd6fb52fb2ef852b63650f4a06787
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-09-30 23:59:35 +0600

    touchableopacity added to screen

--- IUTapp/src/screen/Home.js
+++ IUTapp/src/screen/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, Image,StyleSheet} from 'react-native';
+import {Text, View, Image,StyleSheet, TouchableOpacity} from 'react-native';
 
 const HomeScreen =()=>{
 	return (
@@ -7,6 +7,12 @@ const HomeScreen =()=>{
 			<Image style={styles.imageStyle}
 			source={require('./../../assets/iut_logo.png')}>
 			</Image>
+			<Text style={styles.textStyle}>Department Of CSE</Text>
+			<Text style={styles.textStyle}>Programme: SWE</Text>
+			<TouchableOpacity style={styles.button}>
+        		<Text style={{fontSize: 20, color: 'black'}}>My Profile</Text>
+      		</TouchableOpacity>
+
 		</View>
 		);
 }
@@ -20,9 +26,16 @@ const styles = StyleSheet.create(
 	    height: "50%",
 	    textAlign: "center"
 	  },
+
+	    button: {
+	    	width: 300,
+		    alignItems: "center",
+		    backgroundColor: "#DDDDDD",
+		    padding: 10
+		  },
 		textStyle:{
-			fontSize: 30,
-			color: 'blue',
+			fontSize: 15,
+			color: 'black',
 		},
 
 		imageStyle:{
