8eb64d7324bb0dab7c6dc0997a6e42e7be517883
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-10-01 12:06:10 +0600

    Profile added

--- IUTapp/src/screen/Profile.js
+++ IUTapp/src/screen/Profile.js
@@ -3,10 +3,29 @@ import {Text, View, Image,StyleSheet, TouchableOpacity} from 'react-native';
 
 const ProfileScreen=()=>{
     return(
-        <View>
-           <Text> Hi new page</Text>
+        <View style={styles.container}>
+           <Image source={require('./../../assets/170042001.jpg')} style={styles.imageStyle}></Image>
+            <Text>Name: Maliha Noushin Raida</Text>
+            <Text>Student Id: 170042001</Text>
+            <Text>Email: malihanoushin@iut-dhaka.edu</Text>
         </View>
     );
 }
+
+const styles=StyleSheet.create(
+    {
+        container: {
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            
+            textAlign: "center"
+        },
+        imageStyle:{
+            height:200,
+            width:150
+        }
+    }
+);
 
 export default ProfileScreen;
